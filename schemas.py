0a7ef283426cff2cce01bdab996e54ec489613f0
Douglas Cardoso <douglascardoso@cos.ufrj.br>
2024-06-07 16:07:18 +0100

    Minor schemas adjustment

--- schemas.py
+++ schemas.py
@@ -21,11 +21,11 @@ class PlainUserSchema(LoginSchema):
 
 class CorpusSchema(PlainCorpusSchema):
     documents = fields.List(fields.Nested(PlainDocumentSchema()), dump_only=True)
-    user_id = fields.Int(required=True, load_only=True)
+    user_id = fields.Int(required=True, dump_only=True)
     user = fields.Nested(PlainUserSchema(), dump_only=True)
 
 class DocumentSchema(PlainDocumentSchema):
-    corpus_id = fields.Int(required=True, load_only=True)
+    corpus_id = fields.Int(required=True)
     corpus = fields.Nested(PlainCorpusSchema(), dump_only=True)
 
 class UserSchema(PlainUserSchema):
