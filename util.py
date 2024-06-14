21f471090cd791d4994f8e24e38db69f74c48a37
Douglas Cardoso <douglascardoso@cos.ufrj.br>
2024-06-14 18:17:33 +0100

    Improving organization of general utility functions

--- util.py
+++ util.py
@@ -1,6 +1,10 @@
 from flask_smorest import abort
 from functools import wraps
-from flask_jwt_extended import verify_jwt_in_request, get_jwt
+from flask_jwt_extended import verify_jwt_in_request, get_jwt, get_jwt_identity
+from models import RevokedJWT
+from db import db
+import time
+from models import User
 
 def login_required(min_access_level=0):
     def wrapper(fn):
@@ -15,3 +19,15 @@ def login_required(min_access_level=0):
         return decorator
 
     return wrapper
+
+def revoke_jwt():
+    revoked_jwt = RevokedJWT(
+        jti=get_jwt()['jti'],
+        timestamp=time.time(),
+    )
+    db.session.add(revoked_jwt)
+
+    return revoked_jwt
+
+def get_current_user():
+    return db.session.query(User).get(get_jwt_identity())
